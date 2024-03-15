1ee0d4f65c64e93dbbb52f2b54021aeddebcbdcb
5amCurfew <samueltobyknight@gmail.com>
2024-03-15 00:29:05 +0000

    initial commit

---
+++ Makefile
@@ -0,0 +1,6 @@
+venv:
+	@echo "Creating virtual environment..."
+	python3 -m venv venv && \
+	source venv/bin/activate && \
+	python3 -m pip install -r requirements.txt && \
+	which python3;
---
+++ pipeline.py
@@ -0,0 +1,90 @@
+import argparse
+import datetime
+import logging
+from typing import Optional
+import apache_beam as beam
+from apache_beam.options.pipeline_options import PipelineOptions
+
+# User defined functions should always be subclassed from DoFn. This function transforms
+# each element into a tuple where the first field is userId and the second is click. It
+# assigns the timestamp to the metadata of the element such that window functions can use
+# it later to group elements into windows.
+class AddTimestampDoFn(beam.DoFn):
+    def process(self, element):
+        unix_timestamp = element["recordedAt"]
+        element = (element["userId"], {"event": element["event"], "recordedAt": element["event"]})
+
+        # Each element of the pipeline is transformed into a tuple where the first field is the userId 
+        # (which will be assumed the Key in CombinePerKey)
+        yield beam.transforms.window.TimestampedValue(element, unix_timestamp)
+
+class ExtractSessionID(beam.DoFn):
+    def process(self, aggregated_element, window=beam.DoFn.WindowParam):
+        session_id = {
+            "userId": aggregated_element[0],
+            "sessionStartedAt": window.start.to_utc_datetime().strftime("%Y-%m-%d %H:%M:%S"), 
+            "sessionEndedAt": window.end.to_utc_datetime().strftime("%Y-%m-%d %H:%M:%S")
+        }
+        yield (session_id, {"total": aggregated_element[1]})
+
+def build_run(pipeline_options: Optional[PipelineOptions] = None, known_args: Optional[dict] = None, other_args: Optional[list] = None):
+    with beam.Pipeline(options=pipeline_options) as p:
+
+        if known_args.new_user is None:
+            logging.info('No --new-user provided. Using default value None')
+        (
+            p 
+            | "Create" >> beam.Create(
+                [
+                    {"userId": "Andy", "event": "click", "recordedAt": 1603112520},  # Monday, 19 October 2020 13:02:00
+                    {"userId": "Sam", "event": "click", "recordedAt": 1603113240},   # Monday, 19 October 2020 13:14:00
+                    {"userId": "Andy", "event": "click", "recordedAt": 1603113600},  # Monday, 19 October 2020 13:20:00
+                    {"userId": "Andy", "event": "click", "recordedAt": 1603115820},  # Monday, 19 October 2020 13:57:00
+                    {"userId": known_args.new_user, "event": "click", "recordedAt": int(datetime.datetime.now().strftime('%s'))},  # "Now"
+                ]
+            )
+            # Assign timestamp to metadata of elements such that Beam's window functions can
+            # access and use them to group events.
+            | "AddTimestamp" >> beam.ParDo(AddTimestampDoFn())
+            | "Session Window" >> beam.WindowInto(
+                # Each session must be separated by a time gap of at least 30 minutes (1800 sec)
+                beam.transforms.window.Sessions(gap_size=30 * 60), # This is a class extended from WindowFn
+
+                # Triggers determine when to emit the aggregated results of each window. Default
+                # trigger outputs the aggregated result when it estimates all data has arrived,
+                # and discards all subsequent data for that window.
+                # https://medium.com/@shlomisderot/apache-beam-windows-late-data-and-triggers-e2e856c502b9
+                trigger=None,
+
+                # Since a trigger can fire multiple times, the accumulation mode determines
+                # whether the system accumulates the window panes as the trigger fires, or
+                # discards them.
+                accumulation_mode=None,
+
+                # Policies for combining timestamps that occur within a window. Only relevant if
+                # a grouping operation is applied to windows.
+                timestamp_combiner=None,
+
+                # By setting allowed_lateness we can handle late data. If allowed lateness is
+                # set, the default trigger will emit new results immediately whenever late
+                # data arrives.
+                allowed_lateness=beam.transforms.window.Duration(seconds=1 * 24 * 60 * 60),  # 1 day
+            )
+            | "Group By Key/Window" >> beam.combiners.Count.PerKey()
+            | "Extract Session ID" >> beam.ParDo(ExtractSessionID())
+            | "Print" >> beam.ParDo(print)
+        )
+
+if __name__ == "__main__":
+    logging.getLogger().setLevel(logging.INFO)
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--new-user", default=None, help="Parse Arguments using argparse")
+    known_args, other_args = parser.parse_known_args()
+
+    pipeline_options = PipelineOptions(runner="DirectRunner", save_main_session=True, setup_file="./setup.py")
+    build_run(
+        pipeline_options=pipeline_options,
+        known_args=known_args,
+        other_args=other_args,
+    )
